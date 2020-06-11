b06b75e6dcbc5c952d5cce25846aa7cc475102cf
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2020-06-11 20:47:52 +0300

    UsersApi: removed excess f-string

--- rf_api_client/api/users_api.py
+++ rf_api_client/api/users_api.py
@@ -4,7 +4,7 @@ from rf_api_client.models.users_api_models import CurrentUserDto
 
 class UsersApi(BaseApi):
     async def get_current(self) -> CurrentUserDto:
-        url = self.context.base_url / f'api/user'
+        url = self.context.base_url / 'api/user'
 
         async with self.session.get(url) as resp:
             body = await resp.json()
