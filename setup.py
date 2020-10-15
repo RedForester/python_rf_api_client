db784afca9ebd0a15499a99a2689185cd0870624
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2020-10-15 12:10:38 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='rf_api_client',
-    version='0.1.1',
+    version='0.1.2',
     description='RedForester API client',
     long_description=long_description,
     long_description_content_type='text/markdown',
