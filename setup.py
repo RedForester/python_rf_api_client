5145f4c952268aeaa0cc4182b6c5558226ac7cd3
deissh <deissh@yandex.ru>
2020-12-02 13:23:26 +0300

    chore: version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='rf_api_client',
-    version='0.1.3',
+    version='0.1.4',
     description='RedForester API client',
     long_description=long_description,
     long_description_content_type='text/markdown',
