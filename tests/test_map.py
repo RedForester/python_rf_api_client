6e263dd31017ebbafbd0f50dee637ef2e54dbafb
Alexander Shilov <aleksandr.schilow2012@gmail.com>
2020-12-15 20:17:30 +0300

    Split search tests into multiple methods

--- tests/test_map.py
+++ tests/test_map.py
@@ -4,6 +4,7 @@ from typing import List
 import pytest
 
 from rf_api_client import RfApiClient
+from rf_api_client.models.maps_api_models import MapDto
 from rf_api_client.models.nodes_api_models import CreateNodeDto, CreateNodePropertiesDto, PositionType, \
     CreateNodeLinkDto, NodeUpdateDto, PropertiesUpdateDto, GlobalPropertyUpdateDto, NodeDto, NodeTreeDto
 from tests.conftest import Secret
@@ -120,6 +121,13 @@ async def test_partial_load(secret: Secret, api: RfApiClient):
 
 @pytest.mark.asyncio
 async def test_search_nodes(secret: Secret, api: RfApiClient):
+    m = await prepare_search_map(api, secret)
+    await search_simple_test(api, m)
+    await search_advanced_test(api, m)
+    await search_aggregation_test(api, m)
+
+
+async def prepare_search_map(api, secret) -> MapDto:
     m = await prepare_map(api, secret.developer_prefix, 'test_search_nodes')
 
     p = CreateNodePropertiesDto.empty()
@@ -144,13 +152,18 @@ async def test_search_nodes(secret: Secret, api: RfApiClient):
 
     # rf must have time to index new nodes
     await asyncio.sleep(5)
+    return m
 
+
+async def search_simple_test(api: RfApiClient, m: MapDto):
     result = await api.maps.search_nodes('node', [m.id])
     assert len(result) > 0
 
     result = await api.maps.search_nodes('first', [m.id])
     assert len(result) > 0
 
+
+async def search_advanced_test(api: RfApiClient, m: MapDto):
     result = await api.maps.search_nodes_advanced(
         {
             "query_string": {
@@ -171,6 +184,8 @@ async def test_search_nodes(secret: Secret, api: RfApiClient):
     )
     assert len(result) > 0
 
+
+async def search_aggregation_test(api: RfApiClient, m: MapDto):
     result = await api.maps.search_nodes_aggregation(
         query={
             "match_all": {},
